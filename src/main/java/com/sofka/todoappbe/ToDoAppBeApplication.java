837ddf6711a51d4ce86f2e2c369986785b564feb
Jhonny Castro Clavijo <johnny.castro@misena.edu.co>
2022-05-05 09:46:35 -0500

    Todo App Backend Using Java Spring Boot Lombok MySQL Application properties defined Note model created

---
+++ src/main/java/com/sofka/todoappbe/ToDoAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.todoappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppBeApplication.class, args);
+	}
+
+}
